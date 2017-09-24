75c9abcd26af50657689934bbd2f417c75ec7f72
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-09-24 21:03:35 +0800

    Added Sprite class

---
+++ fireflies/src/com/crackerjacks/game/core/animator/Sprite.java
@@ -0,0 +1,72 @@
+package com.crackerjacks.game.core.animator;
+
+import java.awt.*;
+import java.util.LinkedList;
+
+public class Sprite {
+
+    private LinkedList<Point> points;
+
+    private int width;
+
+    private int height;
+
+    private long lastUpdate;
+
+    private int currentListOffset;
+
+    private long duration;
+
+    public Sprite(int width, int height, long duration) {
+        this.height = height;
+        this.width = width;
+        this.duration = duration;
+        points = new LinkedList<>();
+        lastUpdate = 0;
+        currentListOffset = 0;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public void setLastUpdate(long lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public void addPoint(Point point) {
+        points.add(point);
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+
+    public long getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public LinkedList<Point> getPoints() {
+        return points;
+    }
+
+    public void setCurrentListOffset(int currentListOffset) {
+        this.currentListOffset = currentListOffset;
+    }
+
+    public int getCurrentListOffset() {
+        return currentListOffset;
+    }
+}
