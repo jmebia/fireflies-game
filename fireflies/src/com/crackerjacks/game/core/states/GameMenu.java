4c330411664e9b0b88cd768012479d386a0a6d16
Josiah Maius S. Ebia <josiah_ebia@yahoo.com>
2017-09-25 09:49:39 +0800

    Added game menu class

---
+++ fireflies/src/com/crackerjacks/game/core/states/GameMenu.java
@@ -0,0 +1,20 @@
+package com.crackerjacks.game.core.states;
+
+public class GameMenu extends GameState {
+
+    @Override
+    void onEnter() {
+
+    }
+
+    @Override
+    void update(long time) {
+
+    }
+
+    @Override
+    void onExit() {
+
+    }
+    
+}
